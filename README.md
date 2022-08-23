1b941665309eb6d0217c92f1ca0f92f14e3edf05
Muzammil Bilwani <muzammil@brandver.se>
2022-08-23 11:49:20 +0500

    Added num keyboard docs

---
+++ README.md
@@ -0,0 +1,52 @@
+# numeric_keyboard
+
+A simple numeric keyboard widget
+
+## Installation
+
+Add `onscreen_num_keyboard: ^1.0.0` in your `pubspec.yaml` dependencies. And import it:
+
+```dart
+import 'package:onscreen_num_keyboard/onscreen_num_keyboard.dart';
+```
+
+## How to use
+
+Simply create a `NumericKeyboard` widget and pass the required params:
+
+```dart
+NumericKeyboard(
+  onKeyboardTap: _onKeyboardTap
+)
+
+_onKeyboardTap(String value) {
+  setState(() {
+    text = text + value;
+  });
+}
+```
+
+## Params
+
+```dart
+NumericKeyboard(
+  onKeyboardTap: _onKeyboardTap,
+  textStyle: TextStyle(
+    fontSize: 20.0,
+    color: Colors.black
+  ),
+  rightButtonFn: () {
+    setState(() {
+      text = text.substring(0, text.length - 1);
+    });
+  },
+  rightIcon: Icon(Icons.backspace, color: Colors.red,),
+  leftButtonFn: () {
+    print('left button clicked');
+  },
+  leftIcon: Icon(Icons.check, color: Colors.red,),
+  mainAxisAlignment: MainAxisAlignment.spaceEvenly
+)
+```
+
+If something is missing, feel free to open a ticket or contribute!
---
+++ lib/onscreen_num_keyboard.dart
@@ -0,0 +1,121 @@
+library numeric_keyboard;
+
+import 'package:flutter/material.dart';
+
+typedef KeyboardTapCallback = void Function(String text);
+
+class NumericKeyboard extends StatefulWidget {
+  /// Color of the text [default = Colors.black]
+  final TextStyle textStyle;
+
+  /// Display a custom right icon
+  final Widget? rightIcon;
+
+  /// Action to trigger when right button is pressed
+  final Function()? rightButtonFn;
+
+  /// Display a custom left icon
+  final Widget? leftIcon;
+
+  /// Action to trigger when left button is pressed
+  final Function()? leftButtonFn;
+
+  /// Callback when an item is pressed
+  final KeyboardTapCallback onKeyboardTap;
+
+  /// Main axis alignment [default = MainAxisAlignment.spaceEvenly]
+  final MainAxisAlignment mainAxisAlignment;
+
+  const NumericKeyboard(
+      {Key? key,
+      required this.onKeyboardTap,
+      this.textStyle = const TextStyle(color: Colors.black),
+      this.rightButtonFn,
+      this.rightIcon,
+      this.leftButtonFn,
+      this.leftIcon,
+      this.mainAxisAlignment = MainAxisAlignment.spaceBetween})
+      : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() {
+    return _NumericKeyboardState();
+  }
+}
+
+class _NumericKeyboardState extends State<NumericKeyboard> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.only(left: 32, right: 32, top: 20),
+      alignment: Alignment.center,
+      child: Column(
+        children: <Widget>[
+          ButtonBar(
+            alignment: widget.mainAxisAlignment,
+            children: <Widget>[
+              _calcButton('1'),
+              _calcButton('2'),
+              _calcButton('3'),
+            ],
+          ),
+          ButtonBar(
+            alignment: widget.mainAxisAlignment,
+            children: <Widget>[
+              _calcButton('4'),
+              _calcButton('5'),
+              _calcButton('6'),
+            ],
+          ),
+          ButtonBar(
+            alignment: widget.mainAxisAlignment,
+            children: <Widget>[
+              _calcButton('7'),
+              _calcButton('8'),
+              _calcButton('9'),
+            ],
+          ),
+          ButtonBar(
+            alignment: widget.mainAxisAlignment,
+            children: <Widget>[
+              InkWell(
+                  borderRadius: BorderRadius.circular(45),
+                  onTap: widget.leftButtonFn,
+                  child: Container(
+                      alignment: Alignment.center,
+                      width: 50,
+                      height: 50,
+                      child: widget.leftIcon)),
+              _calcButton('0'),
+              InkWell(
+                  borderRadius: BorderRadius.circular(45),
+                  onTap: widget.rightButtonFn,
+                  child: Container(
+                      alignment: Alignment.center,
+                      width: 50,
+                      height: 50,
+                      child: widget.rightIcon))
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _calcButton(String value) {
+    return InkWell(
+        borderRadius: BorderRadius.circular(45),
+        onTap: () {
+          widget.onKeyboardTap(value);
+        },
+        child: Container(
+          alignment: Alignment.center,
+          width: 50,
+          height: 50,
+          child: Text(
+            value,
+            style: widget.textStyle,
+          ),
+        ));
+  }
+}
